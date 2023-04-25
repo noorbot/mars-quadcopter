9b82f8c520e73e3a406ffb60f87869ba4ba4bfb2
noorbot <noor.khabbaz@ontariotechu.net>
2023-04-25 11:34:56 -0400

    updated with open3d 17

--- src/camera/visualize.py
+++ src/camera/visualize.py
@@ -2,7 +2,6 @@ import open3d as o3d
 import numpy as np
 
 print("Load a ply point cloud, print it, and render it")
-pcd = o3d.io.read_point_cloud("src/camera/first pic.ply")
-print(pcd)
-print(np.asarray(pcd.points))
+sample_ply_data = o3d.data.PLYPointCloud()
+pcd = o3d.io.read_point_cloud("src/camera/pics/first pic.ply")
 o3d.visualization.draw_geometries([pcd])
