5895257d2fa061c16a0e721d722b69bc96775ae9
noorbot <noor.khabbaz@ontariotechu.net>
2023-07-24 15:37:01 -0400

    remove ttb points working static

--- src/camera/static_o3d_tests.py
+++ src/camera/static_o3d_tests.py
@@ -1,6 +1,7 @@
 import open3d as o3d
 import numpy as np
 from scipy.spatial.transform import Rotation as R
+import pandas as pd
 
 def convert_to_transfromation_matrix(trans, rot):
     r = R.from_rotvec(rot)        # convert RPY to rotation matrix
@@ -13,7 +14,7 @@ if __name__ == "__main__":
 
     pcd = o3d.io.read_point_cloud("camera/pics/ttb.ply")
 
-    T = convert_to_transfromation_matrix([0,0,-1.3], [3.14,0,0])
+    T = convert_to_transfromation_matrix([0,0,1.2], [0,0,0]) #[3.14,0,0])
     #T = np.array([(3.14,0,0,0),(0,1,0,0),(0,0,1.57,0),(0,0,0,1)])
     pcd_trans = pcd.transform(T)
 
@@ -21,27 +22,39 @@ if __name__ == "__main__":
     # voxel downsamploing
     pcd = pcd.voxel_down_sample(voxel_size=0.01)
 
-    points = np.asarray(pcd.points)
-    # points = points[0:5000]
-    print(points.shape)
-    pcd_sel = pcd_trans#.select_by_index(np.where(points[:, 2] < -1)[0])
-
-    #o3d.visualization.draw_geometries([pcd])
-    # o3d.visualization.draw_geometries([pcd_sel])
-
+    points = np.asarray(pcd_trans.points)
+    pcd_sel = pcd_trans.select_by_index(np.where(points[:, 2] < 0.2)[0])
+    points = np.asarray(pcd_sel.points)
+   # print(points.shape)
+    points_df = pd.DataFrame(points)
+    #print(points_df)
 
     # ground plane segmentation
-    plane_model, inliers = pcd_sel.segment_plane(distance_threshold=0.03,
-                                             ransac_n=3,
-                                             num_iterations=1000)
-    [a, b, c, d] = plane_model
-    print(f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
-    print("Displaying pointcloud with planar points in red ...")
+    # plane_model, inliers = pcd_sel.segment_plane(distance_threshold=0.03,
+    #                                          ransac_n=3,
+    #                                          num_iterations=1000)
+    # [a, b, c, d] = plane_model
+    # print(f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
+    # print("Displaying pointcloud with planar points in red ...")
 
-    inlier_cloud = pcd_sel.select_by_index(inliers)
-    inlier_cloud.paint_uniform_color([1.0, 0, 0])
-    outlier_cloud = pcd_sel.select_by_index(inliers, invert=True)
+    # inlier_cloud = pcd_sel.select_by_index(inliers)
+    # inlier_cloud.paint_uniform_color([1.0, 0, 0])
+    # outlier_cloud = pcd_sel.select_by_index(inliers, invert=True)
+
+
+    # ignore points around ttb location (20cm radius)
+    # lets say we have a ttb at ttb_x, ttb_y
+    ttb_x = 0
+    ttb_y = 0
+    points_df_ttb = points_df[(points_df.iloc[:,0]>(ttb_x-0.2)) & (points_df.iloc[:,0]<(ttb_x+0.2)) & (points_df.iloc[:,1]>(ttb_y-0.2)) & (points_df.iloc[:,1]<(ttb_y+0.2))]
+
+    points_np = points_df.to_numpy()
+    cloud_global = o3d.geometry.PointCloud()
+    cloud_global = pcd_sel.select_by_index(points_df_ttb.index, invert=True)
+
+
    
-   
+    o3d.visualization.draw_geometries([cloud_global])
+    # o3d.visualization.draw_geometries([pcd_sel])
     # visualize output
-    o3d.visualization.draw([inlier_cloud, outlier_cloud])
+    #o3d.visualization.draw([inlier_cloud, outlier_cloud])
