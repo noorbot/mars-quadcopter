d446afdc2ce98c076a654fb2010e7983b303a440
noorbot <noor.khabbaz@ontariotechu.net>
2023-07-25 13:11:26 -0400

    static ttb removal done

--- src/camera/static_o3d_tests.py
+++ src/camera/static_o3d_tests.py
@@ -25,36 +25,33 @@ if __name__ == "__main__":
     points = np.asarray(pcd_trans.points)
     pcd_sel = pcd_trans.select_by_index(np.where(points[:, 2] < 0.2)[0])
     points = np.asarray(pcd_sel.points)
-   # print(points.shape)
     points_df = pd.DataFrame(points)
-    #print(points_df)
-
-    # ground plane segmentation
-    # plane_model, inliers = pcd_sel.segment_plane(distance_threshold=0.03,
-    #                                          ransac_n=3,
-    #                                          num_iterations=1000)
-    # [a, b, c, d] = plane_model
-    # print(f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
-    # print("Displaying pointcloud with planar points in red ...")
-
-    # inlier_cloud = pcd_sel.select_by_index(inliers)
-    # inlier_cloud.paint_uniform_color([1.0, 0, 0])
-    # outlier_cloud = pcd_sel.select_by_index(inliers, invert=True)
-
 
     # ignore points around ttb location (20cm radius)
     # lets say we have a ttb at ttb_x, ttb_y
     ttb_x = 0
     ttb_y = 0
     points_df_ttb = points_df[(points_df.iloc[:,0]>(ttb_x-0.2)) & (points_df.iloc[:,0]<(ttb_x+0.2)) & (points_df.iloc[:,1]>(ttb_y-0.2)) & (points_df.iloc[:,1]<(ttb_y+0.2))]
+    pcd_sel = pcd_sel.select_by_index(points_df_ttb.index, invert=True)
 
-    points_np = points_df.to_numpy()
-    cloud_global = o3d.geometry.PointCloud()
-    cloud_global = pcd_sel.select_by_index(points_df_ttb.index, invert=True)
+    # ground plane segmentation
+    plane_model, inliers = pcd_sel.segment_plane(distance_threshold=0.03,
+                                             ransac_n=3,
+                                             num_iterations=1000)
+    [a, b, c, d] = plane_model
+    print(f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
+    print("Displaying pointcloud with planar points in red ...")
+
+    inlier_cloud = pcd_sel.select_by_index(inliers)
+    inlier_cloud.paint_uniform_color([1.0, 0, 0])
+    outlier_cloud = pcd_sel.select_by_index(inliers, invert=True)
+
+
+
 
 
    
-    o3d.visualization.draw_geometries([cloud_global])
+    #o3d.visualization.draw_geometries([cloud_global])
     # o3d.visualization.draw_geometries([pcd_sel])
     # visualize output
-    #o3d.visualization.draw([inlier_cloud, outlier_cloud])
+    o3d.visualization.draw([inlier_cloud, outlier_cloud])
