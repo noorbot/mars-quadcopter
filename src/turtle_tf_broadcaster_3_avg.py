8e1f8a6c24e864f6cf77718cf79448465bef2741
noorbot <noor.khabbaz@ontariotechu.net>
2024-02-10 11:22:43 -0500

    take average of aruco readings

--- src/turtle_tf_broadcaster_3_avg.py
+++ src/turtle_tf_broadcaster_3_avg.py
@@ -10,13 +10,13 @@ import math
 import pandas as pd
 
 avgArucoPose = PoseStamped()
-ttbPose = PoseStamped()    # PoseStamped object to be published as the aruco position
+ttbPose = PoseStamped()    # PoseStamped object to be published as the UGV position
 do_once = True               # variable to ensure the map merging parameters are only set once
 
 
-aruco_df = pd.DataFrame(columns = ['x', 'y', 'z', 'orientation.x', 'orientation.y', 'orientation.z', 'orientation.w' ])
+aruco_df = pd.DataFrame(columns = ['x', 'y', 'z', 'orientation.x', 'orientation.y', 'orientation.z', 'orientation.w' ]) # dataframe to hold all aruco readings
 
-conditionMet = False
+conditionMet = False    # whether we have sufficient aruco readings
 firstReading = True
 old_value = [0,0,0]
 start = rospy.Time(5.0)
@@ -32,16 +32,14 @@ def listen_aruco_pose(trans, rot):
         global old_value
         global start
         
-        if (firstReading):
-            print(old_value)
+        if (firstReading):    # first time through, set start time to now
             start = rospy.get_rostime()
             firstReading = False
-            print("FIRST READING")
 
         new_value = trans
-        if (new_value != old_value):
+        if (new_value != old_value):  # if the aruco position reading is new, made a new row with the pose information
 
-            list = [trans[0], 
+            row = [trans[0], 
                     trans[1],
                     trans[2],
                     rot[0],
@@ -50,17 +48,15 @@ def listen_aruco_pose(trans, rot):
                     rot[3]
             ]
         
-            aruco_df.loc[len(aruco_df)] = list
-            print((rospy.get_rostime() - start))
-        
+            aruco_df.loc[len(aruco_df)] = row   # add row to aruco_df dataframe
 
-        old_value = new_value
+        old_value = new_value  # update
 
         if (len(aruco_df.index) > 10 and (rospy.get_rostime() - start) > rospy.Duration(5)):  # if we have more than 10 aruco readings and over 5 seconds has passed since the first one
-            print(aruco_df)
-            conditionMet = True
+            #print(aruco_df)
+            conditionMet = True    # update variable to say, we have enough readings and now we can take the averages and then publish them
 
-            avgArucoPose.pose.position.x = aruco_df.loc[:, 'x'].mean()
+            avgArucoPose.pose.position.x = aruco_df.loc[:, 'x'].mean()   # take the mean of each column of aurco_df adn save to avgArucoPose variable
             avgArucoPose.pose.position.y = aruco_df.loc[:, 'y'].mean()
             avgArucoPose.pose.position.z = aruco_df.loc[:, 'z'].mean()
             avgArucoPose.pose.orientation.x = aruco_df.loc[:, 'orientation.x'].mean()
@@ -71,7 +67,7 @@ def listen_aruco_pose(trans, rot):
 
     return avgArucoPose, conditionMet
 
-# callback function for the location of the aruco marker
+# function to calculate the UGV pose
 def calc_ttb_pose(avgArucoPose):
 
     global ttbPose
@@ -92,7 +88,7 @@ def calc_ttb_pose(avgArucoPose):
 def turtle_tf_broadcaster():
 
     # initialize node
-    rospy.init_node('turtle_tf_broadcaster_1')
+    rospy.init_node('turtle_tf_broadcaster_3')
 
     global ttbPose
     global conditionMet
@@ -103,27 +99,24 @@ def turtle_tf_broadcaster():
     rate = rospy.Rate(100.0)
     while not rospy.is_shutdown():
 
-        if(conditionMet == False):
-            # lookup transform between map and fiducial NEW
+        if(conditionMet == False):    # if we don't yet have enough aruco readings
             try:
                 # lookup transform between map and fiducial
-                (trans,rot) = listener.lookupTransform('/map', '/fiducial_1', rospy.Time(0))
+                (trans,rot) = listener.lookupTransform('/map', '/fiducial_3', rospy.Time(0))
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
                 continue
 
-            avgArucoPose, conditionMet = listen_aruco_pose(trans, rot)
+            avgArucoPose, conditionMet = listen_aruco_pose(trans, rot)  # call function to save the aruco pose information
 
-        if(conditionMet == True):
 
-            
+        if(conditionMet == True):       # if we DO have enough aruco readings
 
-            
             global do_once  # only do once
 
             # set the parameters for map merging initial poses (x, y, z, yaw)
             if do_once == True:
 
-                ttbPose = calc_ttb_pose(avgArucoPose)
+                ttbPose = calc_ttb_pose(avgArucoPose)  # call function to calculate UGV pose
 
                 # Quaternion(w, x, y, z)    MAY WANT TO LEAVE OUT X AND Y
                 # it is necessary to normalize the quaternion before sending the transform
@@ -132,20 +125,21 @@ def turtle_tf_broadcaster():
                 if norm_quat == Quaternion(0,0,0,0): # before the aruco is found, this quaternion throws an error. Set to a default value
                     norm_quat = Quaternion(1,0,0,0)
 
-                rospy.set_param('/robot_2/map_merge/init_pose_x', ttbPose.pose.position.x.item()) # the .item() is needed to convert from numpy float to native python gloat to be accepted
-                rospy.set_param('/robot_2/map_merge/init_pose_y', ttbPose.pose.position.y.item())
-                rospy.set_param('/robot_2/map_merge/init_pose_z', 0.0)
-                rospy.set_param('/robot_2/map_merge/init_pose_yaw' , 0.0) #aruco_yaw)   Set yaw to zero always, as with simulation. Taken care of already 
-                rospy.loginfo("PARAMS SET robot 2: x:" + str(ttbPose.pose.position.x) + "  y:" + str(ttbPose.pose.position.y) + "  z:" + str(ttbPose.pose.position.z) + "  yaw: 0.0")
+                # update ROS parameters regarding UGV starting pose used for map_merging
+                rospy.set_param('/robot_1/map_merge/init_pose_x', ttbPose.pose.position.x.item()) # the .item() is needed to convert from numpy float to native python gloat to be accepted
+                rospy.set_param('/robot_1/map_merge/init_pose_y', ttbPose.pose.position.y.item())
+                rospy.set_param('/robot_1/map_merge/init_pose_z', 0.0)
+                rospy.set_param('/robot_1/map_merge/init_pose_yaw' , 0.0) #aruco_yaw)   Set yaw to zero always, as with simulation. Taken care of already by broadcaster
+                rospy.loginfo("PARAMS SET robot 1: x:" + str(ttbPose.pose.position.x) + "  y:" + str(ttbPose.pose.position.y) + "  z:" + str(ttbPose.pose.position.z) + "  yaw: 0.0")
                 
                 do_once = False
 
-            # publish transform from map to robot_2/map
+            # publish transform for the UGV starting pose [from robot_x_noor/odom (middle step tf which is at same loc as /map) to robot_x/odom]
             br.sendTransform((ttbPose.pose.position.x, ttbPose.pose.position.y, 0.0),
                             (norm_quat.x, norm_quat.y, norm_quat.z, norm_quat.w),
                             rospy.Time.now(),
-                            "robot_2/odom",         
-                            "robot_2_noor/odom")
+                            "robot_1/odom",         
+                            "robot_1_noor/odom")
         rate.sleep()
 
 
